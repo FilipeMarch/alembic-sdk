87f8d4664d47be0a448f21cfd9de44cc4691c988
Filipe Marchesini <filipe.marchesini@gmail.com>
2024-01-02 17:47:23 -0300

    ✨ Added functions for creating database engine and database file

--- alembic_sdk/__init__.py
+++ alembic_sdk/__init__.py
@@ -11,7 +11,6 @@ def remove_migration_directory(directory_name=MIGRATIONS_DIR):
     if os.path.isdir(directory_name):
         logger.debug(f"Removing migration directory at {directory_name}")
         shutil.rmtree(directory_name)
-        logger.debug(f"Removed migration directory at {directory_name}")
     else:
         logger.debug(f"No migration directory found at {directory_name}")
 
@@ -23,7 +22,6 @@ def remove_alembic_ini():
     if os.path.isfile("alembic.ini"):
         logger.debug("Removing alembic.ini file")
         os.remove("alembic.ini")
-        logger.debug("Removed alembic.ini file")
     else:
         logger.debug("No alembic.ini file found")
 
@@ -39,16 +37,45 @@ def create_migration_directory(directory_name=MIGRATIONS_DIR):
     from alembic import command
     from alembic.config import Config
 
-    # Set your database connection string
-    database_url = "sqlite:///example.db"
-
     # Initialize Alembic configuration
     alembic_config = Config()
     alembic_config.set_main_option("script_location", MIGRATIONS_DIR)
-    alembic_config.set_main_option("sqlalchemy.url", database_url)
     alembic_config.config_file_name = "alembic.ini"  # Set the config file name
 
     # Create the Alembic environment
+    logger.debug(f"Creating migration directory at {directory_name}")
     command.init(alembic_config, MIGRATIONS_DIR)
 
-    logger.debug(f"Created new migration directory at {directory_name}")
+
+def create_engine(url, library="sqlalchemy"):
+    """Create a database engine."""
+    import importlib
+
+    module = importlib.import_module(library)
+    return module.create_engine(url)
+
+
+def create_db(url, library="sqlalchemy"):
+    """Create a database."""
+    import importlib
+    import os
+
+    module = importlib.import_module(library)
+    engine = module.create_engine(url)
+
+    if url.startswith("sqlite:///"):
+        # Create local folder for sqlite database
+        folder_name = os.path.dirname(url).replace("sqlite:///", "")
+        if not os.path.exists(folder_name):
+            os.makedirs(folder_name)
+
+    if library == "sqlmodel":
+        from sqlmodel import SQLModel
+
+        SQLModel.metadata.create_all(engine)
+
+    elif library == "sqlalchemy":
+        from sqlalchemy import MetaData
+
+        metadata = MetaData()
+        metadata.create_all(engine)
