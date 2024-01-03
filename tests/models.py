1ec452a886449c07fd0754a0857feabce5f6027f
Filipe Marchesini <filipe.marchesini@gmail.com>
2024-01-02 21:22:11 -0300

    🔥 Remove User model and import_models.py

--- tests/models.py
+++
@@ -1,8 +0,0 @@
-from sqlmodel import Field, SQLModel
-
-
-class User(SQLModel, table=True, extra="allow"):
-    __tablename__ = "users"
-
-    id: int = Field(primary_key=True)
-    name: str = Field(max_length=100)
